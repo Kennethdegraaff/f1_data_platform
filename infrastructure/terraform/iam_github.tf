20ddfb9585dd831a0b8069811edf7181872cdcec
Kennethdegraaff <kenneth.de.graaff@gmail.com>
2026-08-24 14:45:37 +0200

    Add GitHub Actions CI/CD deployment

---
+++ infrastructure/terraform/iam_github.tf
@@ -0,0 +1,99 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+
+  tags = local.common_tags
+}
+
+
+resource "aws_iam_role" "github_actions" {
+  name = "f1-data-platform-github-actions"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:Kennethdegraaff/f1_data_platform:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = local.common_tags
+}
+
+
+resource "aws_iam_policy" "github_actions" {
+  name = "f1-data-platform-github-actions"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+
+        Resource = "*"
+      },
+
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart",
+          "ecr:BatchGetImage"
+        ]
+
+        Resource = aws_ecr_repository.f1_data_platform.arn
+      },
+
+      {
+        Effect = "Allow"
+
+        Action = [
+          "lambda:GetFunction",
+          "lambda:UpdateFunctionCode"
+        ]
+
+        Resource = aws_lambda_function.f1_data_platform.arn
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "github_actions" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions.arn
+}
--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,3 +1,8 @@
 output "ecr_repository_url" {
   value = aws_ecr_repository.f1_data_platform.repository_url
 }
+
+output "github_actions_role_arn" {
+  description = "IAM role ARN used by GitHub Actions"
+  value       = aws_iam_role.github_actions.arn
+}
